b466e6faf1bdf1d482d6c8f6cea8375322f9804b
Amir11f <zohrabiamirhossain@gmail.com>
2024-07-11 17:30:32 +0330

    boxs ui update

--- src/components/boxes/Box.tsx
+++ src/components/boxes/Box.tsx
@@ -3,7 +3,47 @@ import "./Box.css";
 const Box = () => {
   return (
     <>
-      <div className="box"></div>
+      <div className="box">
+        <div className="boxRowUp">
+          <div className="boxRowUpLeft">
+            <div className="fatherRectangle">
+              <div className="rectangleShape" style={{ background: "green" }}>
+                <img src="/svgs/EarthPlanet.svg" alt="" className="EarthSvg" />
+              </div>
+              <div className="rectangleNumber ">
+                <p className="rectangleNumberText">300</p>
+              </div>
+            </div>
+            <div className="underRectangle">
+              <p className="underRectangleText">domain</p>
+            </div>
+          </div>
+          <div className="boxRowUpRight">
+            <img src="/svgs/Arrow.svg" alt="dfs" className="arrow" />
+          </div>
+        </div>
+        <div className="spliterBorder"></div>
+
+        <div className="boxRowMiddle">
+          <div className="middleRight">
+            <div className="chartInfo">
+              <div className="chartInfoTop sameInfo">Live</div>
+              <div className="chartInfoBottom sameInfo">60</div>
+            </div>
+            <img src="/images/dashboardF.png" alt="" className="chartImg" />
+          </div>
+          <div className="middleLeft">
+            <div className="chartInfo">
+              <div className="chartInfoTop sameInfo">Monitored</div>
+              <div className="chartInfoBottom sameInfo">300</div>
+            </div>
+            <img src="/images/dashboardF.png" alt="" className="chartImg" />
+          </div>
+        </div>
+        <div className="spliterBorder"></div>
+
+        <div className="boxRowDown"></div>
+      </div>
     </>
   );
 };
