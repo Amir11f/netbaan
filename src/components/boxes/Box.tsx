88b137f9c5f0d6edbd83461c373b25cbeb1ee56d
Amir11f <zohrabiamirhossain@gmail.com>
2024-07-12 00:47:37 +0330

    box componet ui done

--- src/components/boxes/Box.tsx
+++ src/components/boxes/Box.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import "./Box.css";
 
 const Box = () => {
+  // const [data, setData] = useState({});
+
+  // async function mount() {
+  //   const infos = await fetch("/dummyData/metainfo.json");
+  //   const gett = await JSON.stringify(infos);
+  //   console.log(gett);
+  //   setData(infos);
+  // }
+  // useEffect(() => {
+  //   mount();
+  // }, []);
+  // console.log(data);
+
   return (
     <>
-      <div className="box">
+      <div className="box font">
         <div className="boxRowUp">
           <div className="boxRowUpLeft">
             <div className="fatherRectangle">
@@ -11,11 +25,11 @@ const Box = () => {
                 <img src="/svgs/EarthPlanet.svg" alt="" className="EarthSvg" />
               </div>
               <div className="rectangleNumber ">
-                <p className="rectangleNumberText">300</p>
+                <p className="rectangleNumberText ">300</p>
               </div>
             </div>
-            <div className="underRectangle">
-              <p className="underRectangleText">domain</p>
+            <div className="underRectangle ">
+              <p className="underRectangleText ">Domains</p>
             </div>
           </div>
           <div className="boxRowUpRight">
@@ -27,15 +41,15 @@ const Box = () => {
         <div className="boxRowMiddle">
           <div className="middleRight">
             <div className="chartInfo">
-              <div className="chartInfoTop sameInfo">Live</div>
-              <div className="chartInfoBottom sameInfo">60</div>
+              <p className="chartInfoTop sameInfo InfoTexts">Live</p>
+              <p className="chartInfoBottom sameInfo infoNumbers">60</p>
             </div>
             <img src="/images/dashboardF.png" alt="" className="chartImg" />
           </div>
           <div className="middleLeft">
             <div className="chartInfo">
-              <div className="chartInfoTop sameInfo">Monitored</div>
-              <div className="chartInfoBottom sameInfo">300</div>
+              <p className="chartInfoTop sameInfo InfoTexts">Monitored</p>
+              <p className="chartInfoBottom sameInfo infoNumbers">300</p>
             </div>
             <img src="/images/dashboardF.png" alt="" className="chartImg" />
           </div>
@@ -44,14 +58,32 @@ const Box = () => {
 
         <div className="boxRowDown">
           <div className="downLeft">
-            <div className="downs-left"></div>
+            <div className="downs-left">
+              <img src="/svgs/Globe.svg" alt="" className="img-down" />
+            </div>
             <div className="downs-right">
-              <p className="downs-right-top">IPs</p>
-              <p className="downs-right-bottom">6</p>
+              <p className="downs-right-top InfoTexts sameInfo">IPs</p>
+              <p className="downs-right-bottom infoNumbers sameInfo">6</p>
             </div>
           </div>
-          <div className="downMiddle"></div>
-          <div className="downRight"></div>
+          <div className="downMiddle">
+            <div className="downs-left">
+              <img src="/svgs/Conflict.svg" alt="" className="img-down" />
+            </div>
+            <div className="downs-right">
+              <p className="downs-right-top InfoTexts sameInfo">Ports</p>
+              <p className="downs-right-bottom infoNumbers sameInfo">6</p>
+            </div>
+          </div>
+          <div className="downRight">
+            <div className="downs-left">
+              <img src="/svgs/Bug.svg" alt="" className="img-down" />
+            </div>
+            <div className="downs-right">
+              <p className="downs-right-top InfoTexts sameInfo">Vulns</p>
+              <p className="downs-right-bottom infoNumbers sameInfo">6</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
