4469f031a29aaf21cf6f78f242512de125fa37dd
Amir11f <zohrabiamirhossain@gmail.com>
2024-07-12 01:56:10 +0330

    working on assetComponents

--- src/components/assets/AssetComponents.tsx
+++ src/components/assets/AssetComponents.tsx
@@ -1,5 +1,13 @@
+import "./AssetComponents.css";
 const AssetComponents = () => {
-  return <></>;
+  return (
+    <div className="asset-components">
+      <div className="assetHexagon"></div>
+      <div className="assetName"></div>
+      <div className="assetVulnerabilities"></div>
+      <div className="assetLastSeen"></div>
+    </div>
+  );
 };
 
 export default AssetComponents;
--- src/components/assets/Assets.tsx
+++ src/components/assets/Assets.tsx
@@ -1,8 +1,16 @@
+import AssetComponents from "./AssetComponents";
 import "./Assets.css";
 const Assets = () => {
   return (
-    <div className="assetsContainer">
-      <div></div>
+    <div className="assetsContainer font">
+      <div className="nameText"> Assets</div>
+      <div className="assetPartNames">
+        <div className="partInfo1">Grade</div>
+        <div className="partInfo2">Name</div>
+        <div className="partInfo3">Total Vulnerabilities</div>
+        <div className="partInfo4">Last Seen</div>
+      </div>
+      <AssetComponents />
     </div>
   );
 };
